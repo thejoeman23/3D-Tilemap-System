4945b1c0a2af300d3bfa53b111cd4a32b3f20944
Jonathan Strong <142829586+thejoeman23@users.noreply.github.com>
2025-06-16 11:26:14 -0400

    Update TilemapIcons.cs

--- Editor/TilemapIcons.cs
+++ Editor/TilemapIcons.cs
@@ -5,10 +5,10 @@ public static class TilemapIcons
 {
     // A static class of all tilemap icons any script can call on
     
-    private static Texture2D PaintbrushIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/com.thejoeman23.tilemap3d/Icons/Paintbrush.png");
-    private static Texture2D EraserIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/com.thejoeman23.tilemap3d/Icons/Eraser.png");
-    private static Texture2D BoxFillIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/com.thejoeman23.tilemap3d/Icons/BoxPaint.png");
-    private static Texture2D BoxEraseIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/com.thejoeman23.tilemap3d/Icons/BoxErase.png");
+    private static Texture2D PaintbrushIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.thejoeman23.tilemap3d/Icons/Paintbrush.png");
+    private static Texture2D EraserIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.thejoeman23.tilemap3d/Icons/Eraser.png");
+    private static Texture2D BoxFillIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.thejoeman23.tilemap3d/Icons/BoxPaint.png");
+    private static Texture2D BoxEraseIcon = AssetDatabase.LoadAssetAtPath<Texture2D>("Packages/com.thejoeman23.tilemap3d/Icons/BoxErase.png");
 
     // Returns icon for specific tool
     public static Texture2D GetIcon(ITool tool)
